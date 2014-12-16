70e5f848aff6ef8afad46dd2b08177393faf0371
elperro22 <elperrothehound@hotmail.com>
2014-12-16 11:14:49 +0100

    Añadida clase player-

--- etheria/__init__.py
+++ etheria/__init__.py
@@ -1,14 +1,59 @@
 import socket
+import pygame
+import sys
+from pygame.locals import *
+from random import randint 
+
+# DEFINICION DE FUNCIONES
+def salir():
+    pygame.quit()
+    sys.exit()
+    
+# DEFINICION DE VARIABLES
+estado = "ninguno"
+
+
+# CONFIGURACION INICIAL
+pygame.init()
+ventana = pygame.display.set_mode((1024, 768))
+pygame.display.set_caption("Etheria")
+estado = "login"
+
+
+# BUCLE DE VENTANA
+while True:
+    
+    # EVENTOS
+    for evento in pygame.event.get():
+        if evento.type == QUIT:
+            salir()
+        
+        if evento.type == KEYDOWN and evento.key == K_ESCAPE and estado == "login":
+            salir()
+        
+    # PONEMOS LA IMAGEN DE FONDO
+    fondo = pygame.image.load("images/eter.jpg")        
+    ventana.blit(fondo, (0, 0))
+    
+    # PONEMOS LOS DOS CAMPOS DE TEXTO
+    textUser = pygame.image.load("images/text.png")
+    ventana.blit(textUser, (1024 / 2 - 250, 768 / 2 + 50))
+    textPass = pygame.image.load("images/text.png")
+    ventana.blit(textPass, (1024 / 2 - 250, 768 / 2 - 50))
+    
+    pygame.display.update()
+    
+
   
-s = socket.socket()   
-s.connect(("localhost", 5000))  
+#s = socket.socket()   
+#s.connect(("localhost", 5000))  
   
-while True:  
-    mensaje = raw_input("> ")  
-    s.send(mensaje)  
-    if mensaje == "quit":  
-        break  
+#while True:  
+    #mensaje = raw_input("> ")  
+    #s.send(mensaje)  
+    #if mensaje == "quit":  
+        #break  
   
-print "adios"  
+#print "adios"  
   
-s.close()
+#s.close()
---
+++ etheria/player.py
@@ -0,0 +1,107 @@
+class player:
+    
+    nivel = 0;
+    vida = 0;
+    textura = ""
+    posX = 0
+    posY = 0
+    posO = 0
+    mapa = 0
+    
+    # DEFINICION DE LOS GET SET
+    
+    def getVida(self):
+        return self.vida
+    
+    def setVida(self, vida):
+        self.vida = vida
+        
+    def getNivel(self):
+        return self.nivel
+    
+    def setNivel(self, nivel):
+        self.nivel = nivel
+        
+    def getTextura(self):
+        return self.textura
+    
+    def setTextura(self, textura):
+        self.textura = textura
+        
+    def getPosX(self):
+        return self.posX
+    
+    def setPosX(self, posX):
+        self.posX = posX
+    
+    def getPosY(self):
+        return self.posY
+    
+    def setPosY(self, posY):
+        self.posY = posY
+        
+    def getPosO(self):
+        return self.posO
+    
+    def setPosO(self, posO):
+        self.posO = posO
+        
+    def getMapa(self):
+        return self.mapa
+    
+    def setMapa(self, mapa):
+        self.mapa = mapa
+        
+    
+    # METODOS
+    
+    def mover(self, posO, velocidad):
+        # MOVERSE AL SUR
+        if posO == 0: 
+            self.setY(self.getY() - velocidad)
+        # MOVERSE AL SUROESTE
+        elif posO == 1:
+            self.setX(self.getX() - velocidad)
+            self.setY(self.getY() - velocidad)
+        # MOVERSE AL OESTE
+        elif posO == 2:
+            self.setX(self.getX() - velocidad)
+        # MOVERSE AL NOROESTE
+        elif posO == 3:
+            self.setX(self.getX() - velocidad)
+            self.setY(self.getY() + velocidad)
+        # MOVERSE AL NORTE
+        elif posO == 4:
+            self.setY(self.getY() + velocidad)
+        # MOVERSE AL NORESTE
+        elif posO == 5:
+            self.setX(self.getX() + velocidad)
+            self.setY(self.getY() + velocidad)
+        # MOVERSE AL ESTE
+        elif posO == 6:
+            self.setX(self.getX() + velocidad)
+        # MOVERSE AL SUROESTE
+        elif posO == 7:
+            self.setX(self.getX() + velocidad)
+            self.setY(self.getY() - velocidad)
+            
+        # ASIGNAMOS LA POSICION AL PLAYER
+        self.setPosO(posO)
+            
+    
+    def teletransportar(self, posX, posY, posO, mapa):
+        self.setPosX(posX)
+        self.setPosY(posY)
+        self.setPosO(posO)
+        self.setMapa(mapa)
+        
+        
+    # CONSTRUCTOR
+    
+    def __init__(self):
+        self.setNivel(1)
+        self.setvida(100)
+        self.setTextura("")
+        self.mover(0, 0, 0, 0)
+        
+        
